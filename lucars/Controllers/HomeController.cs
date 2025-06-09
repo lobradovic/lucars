755e918d6cee2748ae74bacc5dbe56e470398350
Luka Obradović <lukaobradovic5@gmail.com>
2025-06-09 02:40:17 +0200

    initial work on homepage

--- lucars/Controllers/HomeController.cs
+++ lucars/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using lucars.Models;
+using lucars.Data;
 
 namespace lucars.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    public readonly ApplicationDbContext context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
     {
+        this.context=context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var automobili = context.Automobils
+            .Include(a => a.Model)
+                .ThenInclude(m => m.Markas)  // ili kako se zove kolekcija/entitet Marka u modelu
+        .Include(a => a.Klasa)
+        .ToList();
+
+        return View(automobili);
     }
 
     public IActionResult Privacy()
